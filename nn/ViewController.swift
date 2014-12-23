e5e64a83f8f700619cc879901ef92fbd1b6f5158
Bent Cardan <bent@nothingsatisfies.com>
2014-12-23 09:27:46 -0500

    IB stuff

--- nn/ViewController.swift
+++ nn/ViewController.swift
@@ -8,11 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+  @IBOutlet weak var simpleTextField: UITextField!
+  
+  @IBOutlet weak var simpleLabel: UILabel!
+  
+  @IBAction func changeLabel(sender: AnyObject) {
+    simpleLabel.text = "Hello, " + simpleTextField.text + "!"
+    self.simpleTextField.resignFirstResponder()
+  }
+  
+  func textFieldShouldReturn(textField: UITextField) -> Bool {
+    simpleLabel.text = "Hello, " + simpleTextField.text + "!"
+    textField.resignFirstResponder()
+    return false
+  }
+  
+  override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+    self.view.endEditing(true)
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //@IBAction this is something the user touches
+    //@IBOutlet some function or thing sets the view object
+    //@IBAction && @IBOutlet
+    
+    
   }
 
   override func didReceiveMemoryWarning() {
